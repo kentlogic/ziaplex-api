ec4e994e97bd3927e38c1b18595522feae66e26b
Vincent Dais <kentlogic@pop-os.localdomain>
2020-10-06 11:40:15 +0800

    Remove nodemon from prod script.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const knex = require("knex");
+const expressip = require("express-ip");
 
+app.use(expressip().getIpInfoMiddleware);
 //Controllers
 const users = require("./controllers/user");
-
+ 
 //Workaround for Error: self signed certificate
 //or code: 'DEPTH_ZERO_SELF_SIGNED_CERT' for the free tier
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
@@ -21,7 +24,9 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({ code: 200, message: `Working.` });
+  const ipInfo = req.ipInfo;
+  console.log(req.ipInfo);
+  res.json({ code: 100, message: `${ipInfo.city}, ${ipInfo.country}` });
 });
 
 app.get("/users", (req, res) => {
@@ -35,6 +40,16 @@ app.post("/user/login", (req, res) => {
 
 app.post("/user/register", (req, res) => {
   users.handleUserRegister(req, res, db);
+});
+
+
+app.put("/user", (req, res) => {
+  users.handleProjectUpdate(req, res, db);
+});
+
+
+app.listen(process.env.PORT || 5001, () => {
+  console.log(`App is listening on port 5001`);
 });
 
 process.on("unhandledRejection", (err) => {
